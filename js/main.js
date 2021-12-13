ae572ec7582a70b7da9f5137294b9bef1f52c42c
Pedram <pedram.pourzeynali@gmail>
2021-12-13 20:22:45 +0330

    'first-coding'

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$(function(){
+		$(".default").datetimepicker();
+	});
+	$(".default").datetimepicker({
+		minuteStep: 1,
+		pickerPosition:'bottom-left',
+//		initialDate:new date(),
+		autoclose:true,
+//		startView: 1,
+		todayBtn:true,
+//		initialDate:new date(),
+		showMeridian:true,
+
+
+
+
+
+
+	});
+
+});
+
+	
